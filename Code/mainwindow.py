703848f0e9d9daa8ae91b6a38ecc9849b147360e
Diana Carrier <umcarr77@gmail.com>
2014-07-02 18:56:24 -0500

    Refactored main window initilization

--- Code/mainwindow.py
+++ Code/mainwindow.py
@@ -1,33 +1,43 @@
 import sys
 sys.path.insert(0, '../Forms/')
 from PyQt4 import QtGui
-from PyQt4.QtGui import QApplication, QMainWindow
+from PyQt4.QtGui import QApplication, QMainWindow, QWidget
 from ui_mainwindow import Ui_MainWindow
 import sqlite3
 
-def btn_addParticipant_clicked(self):
-	print "Participant!"
+app = QApplication(sys.argv)
 
-def connectSlots(ui):
-	ui.btn_addParticipant.clicked.connect(btn_addParticipant_clicked)
+class MainWindow(QWidget):
+	def __init__(self):
+		# Initialize object using ui_mainwindow
+		super(MainWindow, self).__init__()
+		self.window = QMainWindow()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self.window)
+		self.connectSlots()
+		self.initDatabase()
 
-def initDatabase():
-	conn = sqlite3.connect('../Database/AFS')
-	print "Opened database"
-	return conn
+	def initDatabase(self):
+		conn = sqlite3.connect('../Database/AFS')
+		print "Opened database"
 
-def initMainWindow():
-	app = QApplication(sys.argv)
-	window = QMainWindow()
-	ui = Ui_MainWindow()
-	ui.setupUi(window)
-	connectSlots(ui)
-	window.show()
-	sys.exit(app.exec_())
+	def connectSlots(self):
+		self.ui.btn_addParticipant.clicked.connect(self.btn_addParticipant_clicked)
+
+	###### Slots ######
+
+	def btn_addParticipant_clicked(self):
+		print "Participant!"
+
+	def run(self):
+	    # Show the form
+	    self.window.show()
+	    # Run the application
+	    app.exec_()
 
 def main():
-	conn = initDatabase()
-	initMainWindow()
+	myApp = MainWindow()
+	myApp.run()
 
 if __name__ == "__main__":
     main()
