9793695158a0ba5f24a9b2a0795103b410fa9fe7
Diana Carrier <umcarr77@gmail.com>
2014-09-24 20:47:44 -0500

    Fitness function checks for too early start time

--- Code/Testing/testSchedule.py
+++ Code/Testing/testSchedule.py
@@ -3,28 +3,29 @@
 import sys
 sys.path.insert(0, '../')
 import unittest
+import datetime
 
 from schedule import Schedule
 from event import Event
 
 class CheckForOverlappingEventsTests(unittest.TestCase):
-    """tests functions in Schedule"""
+    """tests checkForOverlappingEvents"""
     def setUp(self):
         # Make some Events and fudge the totalTime
         self.e1 = Event("1")
-        self.e1.totalTime = 10
+        self.e1.totalTime = datetime.timedelta(minutes=1)
         self.e2 = Event("2")
-        self.e2.totalTime = 10
+        self.e2.totalTime = datetime.timedelta(minutes=1)
         self.e3 = Event("3")
-        self.e3.totalTime = 10
+        self.e3.totalTime = datetime.timedelta(minutes=1)
     
     def testNoOverlaps(self):
         """test that no overlapping Events returns 0"""
         # Make a Schedule with non-overlapping times
         s = Schedule()
-        s.arrangement.append((0, self.e1))
-        s.arrangement.append((20, self.e2))
-        s.arrangement.append((40, self.e3))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 0), self.e1))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 2), self.e2))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 4), self.e3))
         # Now the actual test
         count = s.checkForOverlappingEvents()
         self.assertEqual(count, 0)
@@ -33,9 +34,9 @@ class CheckForOverlappingEventsTests(unittest.TestCase):
         """test that overlaps occur when an Event starts when the previous one ends"""
         # Make a Schedule with overlapping times
         s = Schedule()
-        s.arrangement.append((0, self.e1))
-        s.arrangement.append((10, self.e2))
-        s.arrangement.append((20, self.e3))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 0), self.e1))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 1), self.e2))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 2), self.e3))
         # Now the actual test
         count = s.checkForOverlappingEvents()
         self.assertEqual(count, 2)
@@ -44,12 +45,50 @@ class CheckForOverlappingEventsTests(unittest.TestCase):
         """test that overlaps are counted properly when events start at the same time"""
         # Make a Schedule with same times
         s = Schedule()
-        s.arrangement.append((0, self.e1))
-        s.arrangement.append((0, self.e2))
-        s.arrangement.append((0, self.e3))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 0), self.e1))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 0), self.e2))
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 0), self.e3))
         # Now the actual test
         count = s.checkForOverlappingEvents()
         self.assertEqual(count, 2)
+
+class IsStartTimeTooEarlyTests(unittest.TestCase):
+    """tests IsStartTimeTooEarly"""
+    def setUp(self):
+        # Make an Event and fudge the totalTime
+        self.e1 = Event("1")
+        self.e1.totalTime = datetime.timedelta(minutes=1)
+
+    def testEarlyTimeIsTrue(self):
+        """a too early start time returns true"""
+        # Make a Schedule
+        s = Schedule()
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 8, 0, 0), self.e1))
+        # Make a startDateTime later than the first Event
+        startDateTime = datetime.datetime(2014, 1, 1, 9, 0, 0)
+        # Test
+        self.assertTrue(s.isStartTimeTooEarly(startDateTime))
+
+    def testOnTimeIsFalse(self):
+        """an on time start time returns false"""
+        # Make a Schedule
+        s = Schedule()
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 9, 0, 0), self.e1))
+        # Make a startDateTime the same time as the first Event
+        startDateTime = datetime.datetime(2014, 1, 1, 9, 0, 0)
+        # Test
+        self.assertFalse(s.isStartTimeTooEarly(startDateTime))
+
+    def testLateTimeIsFalse(self):
+        """a late start time returns false"""
+        # Make a Schedule
+        s = Schedule()
+        s.arrangement.append((datetime.datetime(2014, 1, 1, 10, 0, 0), self.e1))
+        # Make a startDateTime earlier than the first Event
+        startDateTime = datetime.datetime(2014, 1, 1, 9, 0, 0)
+        # Test
+        self.assertFalse(s.isStartTimeTooEarly(startDateTime))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2) # for slightly more detailed results
--- Code/schedule.py
+++ Code/schedule.py
@@ -5,7 +5,7 @@ import sys
 class Schedule(object):
     """Used by the scheduling algorithm"""
     def __init__(self):
-        self.arrangement = [] # list of (time, Event) tuples (remember that these are immutable)
+        self.arrangement = [] # list of (dateTime, Event) tuples (remember that these are immutable)
         # TODO: time can only change +/- 5 minutes (300 seconds)
         # TODO have a init that randomly makes an arrangement?
 
@@ -28,18 +28,37 @@ class Schedule(object):
                     overlapCount += 1
         return overlapCount
 
+    def isStartTimeTooEarly(self, startDateTime):
+        """checks if the arrangement starts too early"""
+        if self.arrangement[0][0] < startDateTime:
+            return True
+        else:
+            return False
+
     def fitness(self):
         """assesses the 'goodness' of the arrangement based on participants not being \
         overbooked (constraint), schools being together, and young kids being in the morning(?)"""
-        # Note: make sure arrangement is sorted by time before beginning
+        # TODO: make sure arrangement is sorted by time before beginning
         
         # Ensure Events do no overlap
         overlapCount = self.checkForOverlappingEvents()
+        # TODO: if overlapCount > 0 mark as infeasible
         # TODO: decrease fitness based on number of overlapping Events
 
-        # Ensure Events do not begin or end during meals
+        # Ensure arrangement does not begin before the start date/time
+        startDateTime = None # TODO: where does this come from
+        if self.isStartTimeTooEarly(startDateTime):
+            # Mark as infeasible
+            # decrease fitness
+            pass
 
-        # Ensure Events are over by a certain time (usually 9pm)
+        # Ensure arrangement does not end after end date/time
+
+        # Ensure Events do not begin or end during lunch time
+
+        # Ensure Events do not begin or end during dinner time
+
+        # Ensure Events do not begin or end at night i.e. (between 9pm one night and 9am the next day)
 
         # Ensure SoloParticipants are not in concsecutive Events
 
